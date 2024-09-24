41ce3cdc318eef20c61980fd4f43647aee04c96f
hazamashoken <earth78203@gmail.com>
2024-09-24 13:37:41 +0800

    add deleted file

--- auth.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [],
-  adapter: FirestoreAdapter(),
-});
