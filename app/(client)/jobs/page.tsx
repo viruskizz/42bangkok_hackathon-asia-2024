7a128da8bc28b01b3f5861a6070180e8ed763c6f
hazamashoken <earth78203@gmail.com>
2024-09-24 10:52:54 +0800

    ground-team: init project module - add shadcn components - setup firebase database - add example test/db/page.tsx

---
+++ app/(client)/jobs/page.tsx
@@ -0,0 +1,14 @@
+export default function JobListPage() {
+    const data = [
+        {
+            name: "job 1",
+            description: "description 1",
+            resturant: "resturant 1",
+
+        }
+    ]
+
+    return (
+        
+    )
+}
