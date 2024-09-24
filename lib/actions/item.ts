191bdac50407e81df484c076e6819c6e70459a18
narin <narin.w@lexnetix.com>
2024-09-24 19:34:24 +0900

    bugfix

--- lib/actions/item.ts
+++ lib/actions/item.ts
@@ -1,24 +1,24 @@
 import axios, { Axios } from "axios";
-import { Order } from "@/lib/types/order";
+import { Item } from "@/lib/types/item";
 
 const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
 });
 
-export async function list(): Promise<Order[]> {
-    const table = "orders.json";
+export async function list(): Promise<Item[]> {
+    const table = "items.json";
     const res = await instance.get(table);
     return res.data;
 }
 
-export async function create(data: Order) {
-    const table = `orders.json`;
+export async function create(data: Item) {
+    const table = `items.json`;
     const res = await instance.post(table, data);
     return res.data;
 }
 
-export async function put(orderId: string , data: Order) {
-    const table = `orders/${orderId}.json`;
+export async function put(orderId: string , data: Item) {
+    const table = `items/${orderId}.json`;
     const res = await instance.put(table, data);
     return res.data;
 }
