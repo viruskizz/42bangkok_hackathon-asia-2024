c17a5ddc93c01346f1f2142746bf62d8ff5bdfb9
narin <narin.w@lexnetix.com>
2024-09-25 00:34:53 +0900

    magic button

--- app/magic-pipeline/page.tsx
+++ app/magic-pipeline/page.tsx
@@ -1,9 +1,9 @@
-"use client"
-import { useState } from 'react';
-import * as batch from '@/lib/actions/batch';
-import * as order from '@/lib/actions/order';
-import * as store from '@/lib/actions/store';
-import * as pipeline from '@/lib/actions/pipeline';
+"use client";
+import { useState } from "react";
+import * as batch from "@/lib/actions/batch";
+import * as order from "@/lib/actions/order";
+import * as store from "@/lib/actions/store";
+import * as pipeline from "@/lib/actions/pipeline";
 import { DateTime } from "luxon";
 
 export default function Page() {
@@ -12,93 +12,67 @@ export default function Page() {
   const listBatchs = async () => {
     const batches = await batch.list();
     const data = [];
-    for(const b of Object.keys(batches)) {
-      data.push(
-        await batch.getBatchDetail(b),
-      )
+    for (const b of Object.keys(batches)) {
+      data.push(await batch.getBatchDetail(b));
     }
     setData(JSON.stringify(data, null, 2));
-  }
+  };
   const createBatch = async () => {
-    const orders = await order.list('WAITING');
+    const orders = await order.list("WAITING");
     console.log(Object.keys(orders));
     const orderIds = Object.keys(orders);
     const data = await batch.createSet(orderIds);
-    setData(JSON.stringify(data, null, 2));
-  }
+    setData("Batch" + JSON.stringify(data, null, 2));
+  };
 
   const listOrders = async () => {
-    const data = await order.list('WAITING');
-    console.log('data:', data);
+    const data = await order.list("WAITING");
+    console.log("data:", data);
     setData(JSON.stringify(data, null, 2));
-  }
+  };
 
   const createOrder = async () => {
-    const storeData = await store.get('Starbuck');
+    const storeData = await store.get("Starbuck");
     const data = await order.create({
-      username: 'Araiva',
+      username: "Araiva",
       store: storeData.name,
       items: storeData.items,
       price: 100,
-      origin: 'bangkok',
-      destination: 'tokyo',
+      origin: "bangkok",
+      destination: "tokyo",
       datetime: DateTime.now().toISO(),
-      status: 'WAITING',
+      status: "WAITING",
     });
-    console.log('data:', data);
+    console.log("data:", data);
     setData(JSON.stringify(data, null, 2));
-  }
+  };
 
   const listPipelines = async () => {
     const data = await pipeline.list();
-    console.log('data:', data);
+    console.log("data:", data);
     setData(JSON.stringify(data, null, 2));
-  }
+  };
 
   const createPipeline = async () => {
     const data = await pipeline.create();
-    console.log('data:', data);
-    setData(JSON.stringify(data, null, 2));
-  }
+    console.log("data:", data);
+    setData("PIPELINE" + JSON.stringify(data, null, 2));
+  };
 
   return (
     <div>
-      <div>
-        <h1 className='text-lg'>Batch</h1>
-        <button
-          className="bg-sky-700 text-white px-4 py-2 rounded-md"
-          onClick={async () => await listBatchs()}>
-          List Batch</button>
-        <button
-          className="bg-yellow-700 text-white px-4 py-2 rounded-md"
-          onClick={async () => await createBatch()}>
-          Create Batch</button>
-      </div>
-      <div>
-        <h1 className='text-lg'>Orders</h1>
-        <button
-          className="bg-sky-700 text-white px-4 py-2 rounded-md"
-          onClick={async () => await listOrders()}>
-          List Orders</button>
-        <button
-          className="bg-yellow-700 text-white px-4 py-2 rounded-md"
-          onClick={async () => await createOrder()}>
-          Create Order</button>
-      </div>
-      <h1 className='text-lg'>Pipeline</h1>
-        <button
-          className="bg-sky-700 text-white px-4 py-2 rounded-md"
-          onClick={async () => await listPipelines()}>
-          List Pipeline</button>
-        <button
-          className="bg-yellow-700 text-white px-4 py-2 rounded-md"
-          onClick={async () => await createPipeline()}>
-          Create Pipeline </button>
-      <div>
-      </div>
+      <button
+        className="bg-yellow-700 text-white px-4 py-2 rounded-md"
+        onClick={async () => {
+          await createBatch();
+          await createPipeline();
+        }}
+      >
+        Magic
+      </button>
       <div>
         <pre>{data}</pre>
       </div>
     </div>
-  )
+  );
 }
