94a3e54e7499bed037dbdde94602ca463560cedc
Tanawat Jukmongkol <tanawat.jukmon@gmail.com>
2024-09-24 23:35:35 +0700

    feature qr-reader

--- app/runner/[id]/accept/page.tsx
+++ app/runner/[id]/accept/page.tsx
@@ -10,7 +10,7 @@ import Image from "next/image";
 import MAP_IMAGE from "@/public/map_mockup.png";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Check } from "lucide-react";
+import { ArrowBigDownDashIcon } from "lucide-react";
 
 export default async function RunnerJobPage({
   params,
@@ -24,7 +24,6 @@ export default async function RunnerJobPage({
     {
       name: "Order 01",
       location: "Location A",
-      dropped: true,
       items: [
         {
           id: "I001",
@@ -50,7 +49,7 @@ export default async function RunnerJobPage({
     },
     {
       name: "Order 02",
-      location: "Location A",
+      location: "Location B",
       items: [
         {
           id: "I001",
@@ -65,41 +64,52 @@ export default async function RunnerJobPage({
     <div className="px-4 py-2">
       <h1 className="text-3xl text-white font-black">Job #{id}</h1>
       <div className="space-y-2">
-        <div className="rounded overflow-hidden">
+        <div className="rounded border-gray-300 border-4 overflow-hidden">
           <Image src={MAP_IMAGE} alt="map" />
         </div>
         {data.map((order) => {
           return (
-            <Card key={order.name} className="bg-white p-2 flex">
-              <div>
-                <h2 className="text-lg font-bold">{order.name}</h2>
-                <p>Location: {order.location}</p>
-                <Collapsible className="space-y-2">
-                  <CollapsibleTrigger>
-                    <p className="underline">Items</p>
-                  </CollapsibleTrigger>
-                  <CollapsibleContent>
-                    {order.items.map((item) => {
-                      return (
-                        <div key={item.id} className="bg-gray-100 p-2">
-                          <h3 className="text-lg font-bold">{item.name}</h3>
-                          <p>Quantity: {item.quantity}</p>
-                        </div>
-                      );
-                    })}
-                  </CollapsibleContent>
-                </Collapsible>
-              </div>
-              {order.dropped && <Check className="text-green-600" size={24} />}
+            <Card key={order.name} className="bg-white p-2">
+              <h2 className="text-lg font-bold">{order.name}</h2>
+              <p>Location: {order.location}</p>
+              <Collapsible className="space-y-2">
+                <CollapsibleTrigger>
+                  <p className="underline">Items</p>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  {order.items.map((item) => {
+                    return (
+                      <div key={item.id} className="bg-gray-100 p-2">
+                        <h3 className="text-lg font-bold">{item.name}</h3>
+                        <p>Quantity: {item.quantity}</p>
+                      </div>
+                    );
+                  })}
+                </CollapsibleContent>
+              </Collapsible>
+			  {/* <div className="flex justify-end">
+				<Button className="bg-pink-500 mt-2">
+					<Link href={`/runner/${id}/dropoff`}>
+						Dropoff <ArrowBigDownDashIcon className="ml-2 mr-0 float-right"/>
+					</Link>
+				</Button>
+			  </div> */}
             </Card>
           );
         })}
       </div>
-      <div className="flex justify-center gap-4 pt-2">
-        <Button variant={"outline"} className="text-lg">
-          <Link href={`/runner/${id}/dropoff`}>DropOff</Link>
-        </Button>
-      </div>
+      {/* <div className="flex justify-center gap-4 pt-2 content-end">
+		<Button variant={"outline"} className="text-lg">
+			<Link href={`/runner/${id}/dropoff`}>DropOff</Link>
+		</Button>
+      </div> */}
+	  <div className="flex justify-end">
+		<Button className="bg-pink-500 mt-2">
+			<Link href={`/runner/${id}/dropoff`}>
+				Dropoff <ArrowBigDownDashIcon className="ml-2 mr-0 float-right"/>
+			</Link>
+		</Button>
+	  </div>
     </div>
   );
 }
